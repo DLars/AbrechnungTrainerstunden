0c613a2fbb8a81a7fde5dcd67f7e67105674c83c
DLars <lars.dubrau@web.de>
2017-12-12 21:54:23 +0100

    initial commit

---
+++ app/src/main/java/abrechnung/larsd/de/abrechnungtrainerstunden/MainActivity.java
@@ -0,0 +1,14 @@
+package abrechnung.larsd.de.abrechnungtrainerstunden;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Hallo Welt
+    }
+}
