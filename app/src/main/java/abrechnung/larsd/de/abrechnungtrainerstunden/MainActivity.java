f0c7765a1010287f6ef4ad75a05cf77dd4e25ad1
DLars <lars.dubrau@web.de>
2017-12-13 21:06:50 +0100

    hinzufügen von ersten datenbak elemnten

--- app/src/main/java/abrechnung/larsd/de/abrechnungtrainerstunden/MainActivity.java
+++ app/src/main/java/abrechnung/larsd/de/abrechnungtrainerstunden/MainActivity.java
@@ -1,15 +1,42 @@
 package abrechnung.larsd.de.abrechnungtrainerstunden;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Hallo Welt hihhiho
-        //TestTest
+
+        FirebaseDatabase md = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = md.getReference();
+
+        DatabaseReference auch = myRef.child("Test");
+        auch.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Toast t = Toast.makeText(getApplicationContext(), dataSnapshot.getValue(String.class), Toast.LENGTH_LONG);
+                t.show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
