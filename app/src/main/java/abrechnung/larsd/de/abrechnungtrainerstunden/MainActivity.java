57d6d1accb82481be6f45ec2fbc5dd8cb01b0694
larsd <lars.dubrau@web.de>
2017-12-16 14:11:40 +0100

    MainActivity fertiggestellt

--- app/src/main/java/abrechnung/larsd/de/abrechnungtrainerstunden/MainActivity.java
+++ app/src/main/java/abrechnung/larsd/de/abrechnungtrainerstunden/MainActivity.java
@@ -1,8 +1,13 @@
 package abrechnung.larsd.de.abrechnungtrainerstunden;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -13,30 +18,54 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //Variablen für Bedienelemente
+    TextView tv_summe, tv_stunden;
+    Button btn_neu, btn_liste, btn_settings;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        FirebaseDatabase md = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = md.getReference();
+    public void initalisiere() {
 
-        DatabaseReference auch = myRef.child("Test");
-        auch.addValueEventListener(new ValueEventListener() {
+        //Variablen IDs zuweisen
+        tv_summe = (TextView) findViewById(R.id.tv_Summe);
+        tv_stunden = (TextView) findViewById(R.id.tv_Stunden);
+        btn_liste = (Button) findViewById(R.id.btn_liste);
+        btn_neu = (Button) findViewById(R.id.btn_neu);
+        btn_settings = (Button) findViewById(R.id.btn_settings);
+
+        //OnClickListener hinzufuegen
+        btn_liste.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Toast t = Toast.makeText(getApplicationContext(), dataSnapshot.getValue(String.class), Toast.LENGTH_LONG);
-                t.show();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Liste.class);
+                startActivity(intent);
             }
         });
 
+        btn_neu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Neu.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        });
+
+        //tvs initalisieren
+        SharedPreferences pref = getSharedPreferences("Speicher", 0);
+        tv_stunden.setText(pref.getString("Stunden", "0 Stunden"));
+        tv_summe.setText(pref.getString("Summe", "0 Euro"));
     }
 }
---
+++ app/src/main/java/abrechnung/larsd/de/abrechnungtrainerstunden/Settings.java
@@ -0,0 +1,13 @@
+package abrechnung.larsd.de.abrechnungtrainerstunden;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
